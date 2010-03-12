5482aab655513514692a4fb7b73f073e23d965c5
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-03-12 15:01:43 +0000

    RDev

---
+++ trunk/ValidatorSDK/Validate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ValidatorCoreLib;
+
+namespace ValidatorSDK
+{
+    public class Validate
+    {
+        public bool bValidationResult{get;set;}
+        public Validate(ValidationData validationData)
+        {
+            bValidationResult = true;
+            foreach (Rule rule in validationData.ruleList.GetRuleList())
+            {
+                if (rule.nRuleType > (int)RuleType.RuleType_MonoRule__First__ && rule.nRuleType < (int)RuleType.RuleType_MonoRule__Last__)
+                {
+                    Context context = validationData.contexts.Get(rule.getEntity(0).PropertyID);
+                    bValidationResult &= rule.validate(context);
+                }            
+            }
+
+        }
+    }
+}
---
+++ trunk/ValidatorSDK/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ValidatorCoreLib;
+
+
+namespace ValidatorSDK
+{
+    
+
+    public class Validator
+    {
+        ValidationFlow flow;
+        public Validator(ValidationFlow flow)
+        {
+            this.flow = flow;
+        }
+        public ValidationResult Validate(ValidationData data)
+        {
+            if (flow.Validate(data.Contexts))
+            {
+                return new ValidationResult("Has NO conflict");
+            }
+            else
+            {
+                return new ValidationResult("Has conflict");
+            }
+        }
+    }
+
+}
