233b0d4e717f07db35c15e132cbd94487b0f6902
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-03-29 05:19:42 +0000

    XML Serialization + UI updates

--- ValidatorCoreLib/Role.cs
+++ ValidatorCoreLib/Role.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+    
 namespace ValidatorCoreLib
 {
-    
-
-    
-
-    
-
     // input:
     //      nRuleID
     //      contextContain  - the path to the compared object in the contextTable[key]
@@ -21,18 +18,31 @@ namespace ValidatorCoreLib
     //      by using the validate(contextTable) the rule detarmain if the object data has conflict     
     //
     // the rule is a set of definitions to check object data
+    [Serializable(), XmlRoot("ValidationRule", Namespace = "ValidatorCoreLib", IsNullable = false)]
     public class ValidationRule
     {
+        [System.Xml.Serialization.XmlIgnoreAttribute]
         public IOperator Operator { get; set; }
+        // Used only for xml Serialization
+        //public string OperatorName { get { return Operator.GetType().ToString(); } set; }
+        public string OperatorName { get; set; }
+
         public string contextContain { get; set; }
         public string key { get; set; }
         public Object ComparedObject { get; set; }
         public int id { get; set; }
 
+        // Used only for xml Serialization
+        public ValidationRule() 
+        {
+            this.Operator = OperatorHelper.GetOperator(this.OperatorName);
+        }
+
         public ValidationRule(int nRuleID, string contextContain, string key, Object comparedObject, IOperator op)
         {
             this.id = nRuleID;
             this.Operator = op;
+            this.OperatorName = op.GetType().ToString();
             this.contextContain = contextContain;
             this.key = key;            
             this.ComparedObject = comparedObject;
