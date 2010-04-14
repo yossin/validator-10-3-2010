bb62d03f8af58a84a9b922f7cff1e32abff947c8
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-04-14 17:05:18 +0000

    14/4 version (start of convention table, role lines to tickets, resolve)

---
+++ trunk/The Validator11/ConvertionGridData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace The_Validator11
+{
+    [Serializable(), XmlRoot("ConvertionGridData", Namespace = "The_Validator11",IsNullable = false)]
+    public class ConvertionGridData
+    {
+//        public List<ValidationFlow> flows = new List<ValidationFlow>();
+
+        public ConvertionGridData() { }
+
+        public void Add()
+        {
+ //           rules.Add(rule);
+        }
+
+        public void Add(String sFrom, String sTo)
+        {
+  //          flows.Add(flow);
+        }
+
+        public void Clear()
+        {
+ //           flows.Clear();
+        }
+    }
+    
+}
