b17ff2cb6bc063912a4aff16d57c0ef3546357b5
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-04-14 17:05:18 +0000

    14/4 version (start of convention table, role lines to tickets, resolve)

---
+++ The Validator11/ConvertionGridRow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ValidatorCoreLib;
+
+namespace The_Validator11
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ConvertionRow : UserControl
+    {
+        public ConvertionRow(string sFrom, string sTo)
+        {
+            InitializeComponent();
+            
+            ConvertionGridRowFrom.Text = sFrom;
+            ConvertionGridRowTo.Text = sTo;
+        }
+    }
+}
