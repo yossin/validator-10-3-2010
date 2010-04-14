ea808a56fa9ec2e4f22f847f6b849ee9540fb975
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-04-14 17:05:50 +0000

    14/4 version (start of convention table, role lines to tickets, resolve)

---
+++ trunk/ValidatorCoreLib/Resolve.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+    
+namespace ValidatorCoreLib
+{
+    [Serializable(), XmlRoot("ValidationResolve", Namespace = "ValidatorCoreLib", IsNullable = false)]
+    public class ValidationResolve
+    {
+        public string contextContain { get; set; }
+        public string key { get; set; }
+        public Object ConflictedObject { get; set; }
+        public int id { get; set; }
+
+        // conflict resolve 
+        public bool AutoResolve { get; set; }
+
+        public List<ValidationResolveObject> ResolveObjects = new List<ValidationResolveObject>();
+
+        // Used only for xml Serialization
+        public ValidationResolve() 
+        {
+        }
+
+        public ValidationResolve(int nRuleID, string contextContain, string key, Object conflictedObject, bool AutoResolve)
+        {
+            this.id = nRuleID;
+            this.contextContain = contextContain;
+            this.key = key;
+            this.ConflictedObject = conflictedObject;
+            this.AutoResolve = AutoResolve;
+        }
+
+        public void AddResolveObjects(ValidationResolveObject resolveObjects)
+        {
+            ResolveObjects.Add(resolveObjects);
+        }
+
+        public void ClearResolveObjects()
+        {
+            ResolveObjects.Clear();
+        }
+    }
+}
---
+++ trunk/ValidatorCoreLib/ResolveObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+    
+namespace ValidatorCoreLib
+{
+    [Serializable(), XmlRoot("ValidationResolveObject", Namespace = "ValidatorCoreLib", IsNullable = false)]
+    public class ValidationResolveObject
+    {
+        public string ResolveStringForUI { get; set; }
+        public Object ResolveObject { get; set; }
+
+        // Used only for xml Serialization
+        public ValidationResolveObject() 
+        {
+        }
+
+        public ValidationResolveObject(string ResolveStringForUI, Object ResolveObject)
+        {
+            this.ResolveStringForUI = ResolveStringForUI;
+            this.ResolveObject = ResolveObject;
+        }
+    }
+}
