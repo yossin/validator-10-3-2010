1cd1633ce6af7e1edf15209a87501816d69bdace
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-04-19 16:38:09 +0000

    19/4 version (convention table, role lines to tickets, resolve)

--- trunk/ValidatorSDK/ValidationData.cs
+++ trunk/ValidatorSDK/ValidationData.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ValidatorCoreLib;
-
+    
 namespace ValidatorSDK
 {
     public class ValidationData
     {
         public ContextTable Contexts { get; set; }
         public ValidationFlow flow { get; set; }
+        public ValidationConvertionItem convertionItem { get; set; }
+        public ConvertionComparedItems convertionComparedItems { get; set; }
 
         private static int ii = 1;
             
@@ -17,12 +19,16 @@ namespace ValidatorSDK
         {
             this.Contexts = new ContextTable();
             this.flow = new ValidationFlow((ii++).ToString(), true);
+            this.convertionItem = new ValidationConvertionItem();
+            convertionComparedItems = new ConvertionComparedItems();
         }
 
-        public ValidationData(ContextTable contexts, ValidationFlow flow)
+        public ValidationData(ContextTable contexts, ValidationFlow flow, ValidationConvertionItem convertionItem)
         {
             this.Contexts = contexts;
             this.flow = flow;
+            this.convertionItem = convertionItem;
+            convertionComparedItems = new ConvertionComparedItems();
         }
 
         public void Add(string key, Object obj)
@@ -40,10 +46,17 @@ namespace ValidatorSDK
             flow.Add(rule);
         }
 
+        public void Add(ValidationConvertionItem convertionItemToAdd)
+        {
+            convertionItem.Add(convertionItemToAdd);
+        }
+
         public void Clear()
         {
             Contexts.Clear();
             flow.Clear();
+            convertionItem.Clear();
+            convertionComparedItems.Clear();
 
         }
     }
