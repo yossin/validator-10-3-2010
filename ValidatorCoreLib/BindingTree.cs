1182cce233779bef76558efc85572dfc7d6abd93
josefn.il <josefn.il@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-05-10 18:13:02 +0000

    using binding table (context & reference) - forgot 2 files

---
+++ ValidatorCoreLib/BindingTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidatorCoreLib
+{
+
+
+    public class BindingContainer
+    {
+        public BindingContainer(Dictionary<string, string> contextBinding, Dictionary<string, string> referenceBinding)
+        {
+            this.contextBinding = contextBinding;
+            this.referenceBinding = referenceBinding;
+        }
+        //definitionKey, string runtimeKey
+        Dictionary<string, string> contextBinding;
+        //referenceKey, string propertyChain
+        Dictionary<string, string> referenceBinding;
+        public Dictionary<string, string> ContextBinding { get { return contextBinding; } }
+        public Dictionary<string, string> ReferenceBinding { get { return referenceBinding; } }
+    }
+
+
+     
+}
