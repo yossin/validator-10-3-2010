4271b49a4388192557b3b5487f059d4af6deff1d
josefn.il <josefn.il@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-05-10 18:13:02 +0000

    using binding table (context & reference) - forgot 2 files

---
+++ trunk/ValidatorCoreLib/PropertySelection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidatorCoreLib
+{
+    public class PropertySelection
+    {
+        // TODO: create object selection by using feilds
+        public PropertySelection(string contextKey, string referenceMeaning)
+        {
+            this.ContextKey = contextKey;
+            this.ReferenceMeaning = referenceMeaning;
+        }
+        public string ReferenceMeaning { get; set; }
+        public string ContextKey { get; set; }
+
+    }
+}
