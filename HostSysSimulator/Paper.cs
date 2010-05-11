bf1baa93059e7cd00b74ff32a85aed40e6584bdc
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-05-11 12:24:30 +0000

    validator-11-5-2010(2)

---
+++ HostSysSimulator/Paper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HostSysSim
+{
+    class Paper
+    {
+        public HostSysSim.Point size{ get; set; }
+        public Paper(String sVal)   // should be in %d,%d format
+        {
+            size = new Point(sVal);
+        }
+        public Paper(int x, int y)
+        {
+            size = new Point(x,y);
+        }
+    }
+}
