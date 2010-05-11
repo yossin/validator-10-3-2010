b0d3673354141f48226dccab73e9d6dfb5356f7f
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-05-11 12:24:48 +0000

    validator-11-5-2010(2)

---
+++ HostSysSimulator/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HostSysSim
+{
+    class Point
+    {
+        public int x{ get; set; }
+        public int y{ get; set; }
+        public Point(String sVal)   // should be in %d,%d format
+        {
+            char[] seperator = {','};
+            string[] sVals = sVal.Split(seperator);
+            if (sVals.Length != 2)
+                x = y = 0;
+            else
+            {
+                x = System.Convert.ToInt32(sVals[0], 10);
+                y = System.Convert.ToInt32(sVals[1], 10);
+            }
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
