353aa769164ecc7bca3d8e6aa9e0dec377706d22
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-05-21 08:24:48 +0000

    YosiZ: 21/5 changes

---
+++ The Validator11/ConvertionClassItem_AddNew.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ValidatorCoreLib;
+
+namespace The_Validator11
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ConvertionClassItem_AddNew : UserControl
+    {
+        TreeViewItem tviParent;
+        public ConvertionClassItem_AddNew(TreeViewItem tvi)
+        {
+            InitializeComponent();
+            tviParent = tvi;
+        }
+
+        private void AddNewObjectRow_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvi = new TreeViewItem();
+            ConvertionClassItem fr = new ConvertionClassItem(ConvertionClassItem.sIgnoredString, @"Attribute Name", tvi);
+            tvi.Header = fr;
+            tvi.IsExpanded = true;
+            tviParent.Items.Insert(tviParent.Items.Count - 1, tvi);
+        }
+    }
+}
