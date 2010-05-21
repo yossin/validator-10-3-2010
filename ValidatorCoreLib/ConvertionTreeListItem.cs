71d5a5b6bdf0059d6ccea86032cf683608f0950c
yosizil <yosizil@24ffe51a-2c65-11df-8b4d-31f007950f82>
2010-05-21 08:28:12 +0000

    YosiZ: 21/5 changes

---
+++ ValidatorCoreLib/ConvertionTreeListItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ValidatorCoreLib
+{
+    [Serializable(), XmlRoot("ConvertionTreeListItem", Namespace = "ValidatorCoreLib", IsNullable = false)]
+    public class ConvertionTreeListItem
+    {
+        public List<ConvertionTree> convertionTreeListItem = new List<ConvertionTree>();
+
+        // Used only for xml Serialization
+        public ConvertionTreeListItem() { }
+
+        public void Add(ConvertionTree convertionItem)
+        {
+            convertionTreeListItem.Add(convertionItem);
+        }
+
+        public void Clear()
+        {
+            convertionTreeListItem.Clear();
+        }
+    }
+    
+}
